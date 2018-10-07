6d6fd2518ef5a0aa636f9e49baede3a0a5f5e549
Daniel Mazur <39432105+DanielMazurFL4RE@users.noreply.github.com>
2018-10-07 20:32:53 +0200

    Update coinData.pas

--- Android/coinCode/coinData.pas
+++ Android/coinCode/coinData.pas
@@ -3,12 +3,13 @@ unit coinData;
 interface
 
 uses System.IOUtils, sysutils, StrUtils,
-  FMX.Graphics, base58, FMX.Dialogs , WalletStructureData;
+  FMX.Graphics, base58, FMX.Dialogs, WalletStructureData;
 
 function CreateCoin(id, x, y: Integer; MasterSeed: AnsiString;
   description: AnsiString = ''): TWalletInfo;
 function getCoinIcon(id: Integer): TBitmap;
 function isValidForCoin(id: Integer; address: AnsiString): Boolean;
+function getURLToExplorer(id: Integer; hash: AnsiString): AnsiString;
 
 type
   coinInfo = record
@@ -16,36 +17,38 @@ type
     displayName: AnsiString;
     name: AnsiString;
     shortcut: AnsiString;
-    WifByte:AnsiString;
+    WifByte: AnsiString;
     p2sh: AnsiString;
     p2pk: AnsiString;
     flag: System.UInt32;
     decimals: smallint;
+    availableFirstLetter: AnsiString;
 
   end;
 
 const
   // all supported coin
   availableCoin: array [0 .. 4] of coinInfo = ((id: 0; displayName: 'Bitcoin';
-    name: 'bitcoin'; shortcut: 'BTC';wifByte: '80'; p2sh: '05'; p2pk: '00';
+    name: 'bitcoin'; shortcut: 'BTC'; WifByte: '80'; p2sh: '05'; p2pk: '00';
 
-    flag: 0; decimals: 8;
+    flag: 0; decimals: 8; availableFirstLetter: '13b';
 
-    ), (id: 1; displayName: 'Litecoin'; name: 'litecoin'; shortcut: 'LTC'; wifByte: 'B0';
-    p2sh: '05'; p2pk: '30';
+    ), (id: 1; displayName: 'Litecoin'; name: 'litecoin'; shortcut: 'LTC';
+    WifByte: 'B0'; p2sh: '05'; p2pk: '30';
 
-    flag: 0; decimals: 8;
+    flag: 0; decimals: 8; availableFirstLetter: 'l3m';
 
-    ), (id: 1; displayName: 'DASH'; name: 'dash'; shortcut: 'DASH';wifByte: 'CC'; p2sh: '10';
-    p2pk: '4c'; flag: 0; decimals: 8;
+    ), (id: 1; displayName: 'DASH'; name: 'dash'; shortcut: 'DASH';
+    WifByte: 'CC'; p2sh: '10'; p2pk: '4c'; flag: 0; decimals: 8;
+    availableFirstLetter: 'X';
 
     ), (id: 1; displayName: 'Bitcoin Cash'; name: 'bitcoincash';
-    shortcut: 'BCH';wifByte: '80'; p2sh: '05'; p2pk: '00';
+    shortcut: 'BCH'; WifByte: '80'; p2sh: '05'; p2pk: '00';
 
-    flag: 0; decimals: 8;
+    flag: 0; decimals: 8; availableFirstLetter: '13pq';
 
-    ), (id: 1; displayName: 'Ethereum'; name: 'ethereum'; shortcut: 'ETH'; wifByte: '';
-    p2pk: '00'; flag: 1; decimals: 18;
+    ), (id: 1; displayName: 'Ethereum'; name: 'ethereum'; shortcut: 'ETH';
+    WifByte: ''; p2pk: '00'; flag: 1; decimals: 18; availableFirstLetter: '0';
 
     )
 
@@ -53,7 +56,29 @@ const
 
 implementation
 
-uses Bitcoin, Ethereum , misc , UHome;
+uses Bitcoin, Ethereum, misc, UHome;
+
+function getURLToExplorer(id: Integer; hash: AnsiString): AnsiString;
+var
+  URL: AnsiString;
+begin
+
+  case id of
+    0:
+      URL := 'https://www.blockchain.com/btc/tx/';
+    1:
+      URL := 'http://explorer.litecoin.net/tx/';
+    2:
+      URL := 'https://chainz.cryptoid.info/dash/tx.dws?';
+    3:
+      URL := 'https://blockchair.com/bitcoin-cash/transaction/';
+    4:
+      URL := 'https://etherscan.io/tx/';
+
+  end;
+
+  result := URL + hash;
+end;
 
 function getCoinIcon(id: Integer): TBitmap;
 begin
@@ -80,6 +105,31 @@ begin
   wipeAnsiString(MasterSeed);
 end;
 
+function checkFirstLetter(id: Integer; address: AnsiString): Boolean;
+var
+  c : Ansichar;
+  position : integer;
+begin
+
+  address := removeSpace(address);
+  if containsText( address , ':') then
+  begin
+    position := pos( ':' ,address );
+    address  := rightStr( address , length(address) - position );
+  end;
+
+
+  for c in availableCoin[id].availableFirstLetter do
+  begin
+
+    if lowercase(address[low(address)]) = lowercase(c) then
+      exit(true);
+
+  end;
+
+  result := false;  
+end;
+
 // check if given address is of given coin
 function isValidForCoin(id: Integer; address: AnsiString): Boolean;
 var
@@ -91,16 +141,40 @@ begin
   if availableCoin[id].flag = 0 then
   begin
 
-    if (id = 3) and (leftstr(address, 12) = 'bitcoincash:') then
+    if (id = 3) then
     begin
-      result := true;
+
+      str := StringReplace(address, ' ', '', [rfReplaceAll]);
+      str := StringReplace(str, 'bitcoincash:', '', [rfReplaceAll]);
+
+      if (str[low(str)] = 'q') or (str[low(str)] = 'p') then
+      begin
+        // isValidBCHCashAddress(Address)
+        result := true;
+      end
+      else
+      begin
+        info := decodeAddressInfo(address, id);
+        if info.scriptType >= 0 then
+          result := true;
+      end;
 
     end
     else
     begin
+    
+      try
+        info := decodeAddressInfo(address, id);
+      except on E: Exception do
+        begin
+          
 
-      info := decodeAddressInfo(address, id);
-      if info.scriptType>=0 then result:=true;
+          exit(false);
+        end;
+      end;
+      
+      if info.scriptType >= 0 then
+        result := true;
 
     end;
 
@@ -111,6 +185,8 @@ begin
     // showmessage(inttostr(length(address)));
     result := ((isHex(rightStr(address, 40))) and (length(address) = 42));
   end;
+
+  result := result and checkFirstLetter( id, address );
 
 end;
 
